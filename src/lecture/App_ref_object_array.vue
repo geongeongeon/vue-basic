0a6cfff0ef3d12a0d66576af00f78166587b7273
geongeongeon <leegeonhee0204@gmail.com>
2025-02-20 22:11:39 +0900

    반응형 기초

---
+++ src/lecture/App_ref_object_array.vue
@@ -0,0 +1,27 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { reactive, ref } from 'vue';
+
+export default {
+	setup() {
+		const count = ref(0);
+		const state = reactive({
+			count,
+		});
+		count.value++;
+		console.log(count.value);
+		console.log(state.count);
+
+		const message = ref('Hello');
+		const arr = reactive([message]);
+		console.log(arr[0].value);
+
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
