5e009ce65ece36e210f942499d41e7798344448c
geongeongeon <leegeonhee0204@gmail.com>
2025-02-20 19:12:16 +0900

    Setup

---
+++ src/lecture/App_Setup.vue
@@ -0,0 +1,18 @@
+<template>
+	<button @click="count++">{{ count }}</button>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const count = ref(0);
+
+		// 템플릿 및 기타 Options API hook에 노출
+		return {
+			count,
+		};
+	},
+};
+</script>
