8b90f2a290127a6d38b3eceff0c84758fcffadb8
geongeongeon <leegeonhee0204@gmail.com>
2025-02-21 14:25:23 +0900

    class와 style 바인딩

---
+++ src/lecture/App_style_binding.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		<div :style="styleObject">
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+			aperiam. Enim, ab. Officiis maiores tempore laudantium laboriosam illum
+			autem tenetur reiciendis eveniet beatae sapiente? Ad obcaecati odit
+			possimus tempora quisquam!
+		</div>
+		<button v-on:click="fontSize--">-</button>
+		<button v-on:click="fontSize++">+</button>
+	</div>
+</template>
+
+<script>
+import { computed, reactive, ref } from 'vue';
+
+export default {
+	setup() {
+		// const styleObject = reactive({
+		// 	color: 'red',
+		// 	fontSize: '13px',
+		// });
+
+		const fontSize = ref(13);
+		const styleObject = computed(() => {
+			return {
+				color: 'red',
+				fontSize: fontSize.value + 'px',
+			};
+		});
+
+		return {
+			styleObject,
+			fontSize,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
